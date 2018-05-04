c2f0cd8f6cbe90c30f84290a287b50fef621e874
spondbob <spondbob@eamca.com>
2018-05-04 12:19:02 +1000

    fixes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = function(schema, options) {
       )
       const data = dataObj ? dataObj.toObject() : {}
       const lastIndex = get(data, 'friendlySlugs.slug.index')
-      const highestIndex = lastIndex === 0 ? 0 : -1
+      const highestIndex = Number.isInteger(lastIndex) ? lastIndex : -1
 
       const index = highestIndex >= 0 ? highestIndex + 1 : 0
       const suffix = index > 0 ? `-${index}` : ``
