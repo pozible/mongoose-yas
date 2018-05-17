880afe141300631e09232b48749e2850dfbb2664
Rizky <rizky@pozible.com>
2018-05-17 12:33:28 +1000

    node 6 support

--- index.js
+++ index.js
@@ -1,54 +1,61 @@
 const get = require('lodash.get')
 const slug = require('slug')
 
-const log = message => console.log('mongoose-yas:', message)
-
 module.exports = function(schema, options) {
-  log('loaded')
-  schema.pre('save', async function() {
-    log('invoked on save')    
-    const slugFrom = get(schema, 'obj.slug.slugFrom')
-    const target = slugFrom ? this[slugFrom] : this.title
-    const slugBase = slug(target).toLowerCase()
+  schema.pre('save', function() {
+    const self = this
+    const slugBase = getSlugBase(self, schema)
 
-    log(get(this, 'friendlySlugs.slug.base') + ' ' + slugBase)
-    if (get(this, 'friendlySlugs.slug.base') === slugBase) return
+    if (get(self, 'friendlySlugs.slug.base') === slugBase) return
 
-    const getScope = () => {
-      const distinctUpTo = get(schema, 'obj.slug.distinctUpTo') || []
-      return distinctUpTo.reduce((obj, key) => {
-        obj[key] = this[key]
-        return obj
-      }, {})
-    }
-
-    const getIndex = async () => {
-      const scope = getScope()
-
-      const dataObj = await this.constructor.findOne(
-        { ...scope, 'friendlySlugs.slug.base': slugBase },
-        { friendlySlugs: 1 },
-        { sort: { 'friendlySlugs.slug.index': -1 } }
-      )
-      const data = dataObj ? dataObj.toObject() : {}
-      const lastIndex = get(data, 'friendlySlugs.slug.index')
-      const highestIndex = Number.isInteger(lastIndex) ? lastIndex : -1
-
-      return highestIndex >= 0 ? highestIndex + 1 : 0
-    }
-
-    const getSuffix = async (index) => {
-      return index > 0 ? `-${index}` : ``
-    }
-
-    const index = await getIndex()
-    const suffix = await getSuffix(index)
-    this.slug = `${slugBase}${suffix}`
-    this.friendlySlugs = {
-      slug: {
-        base: slugBase,
-        index,
+    const query = getQuery(self, schema, slugBase)
+    return queryLastIndex(self, query).then(index => {
+      const suffix = getSuffix(index)
+      self.slug = `${slugBase}${suffix}`
+      self.friendlySlugs = {
+        slug: {
+          base: slugBase,
+          index,
+        }
       }
-    }
+    })
   })
+}
+
+const queryLastIndex = (self, query) => {
+  return self.constructor.findOne(
+    query,
+    { friendlySlugs: 1 },
+    { sort: { 'friendlySlugs.slug.index': -1 } }
+  ).then(getIndex)
+}
+
+const getIndex = (data) => {
+  const lastIndex = get(data, 'friendlySlugs.slug.index')
+  const highestIndex = Number.isInteger(lastIndex) ? lastIndex : -1
+
+  return highestIndex >= 0 ? highestIndex + 1 : 0
+}
+
+const getQuery = (self, schema, slugBase) => {
+  const scope = getScope(self, schema)
+  return Object.assign(scope, { 'friendlySlugs.slug.base': slugBase })
+}
+
+const getSlugBase = (self, schema) => {
+  const slugFrom = get(schema, 'obj.slug.slugFrom')
+  const target = slugFrom ? self[slugFrom] : self.title
+  return slug(target).toLowerCase()
+}
+
+const getScope = (self, schema) => {
+  const distinctUpTo = get(schema, 'obj.slug.distinctUpTo') || []
+  return distinctUpTo.reduce((obj, key) => {
+    obj[key] = self[key]
+    return obj
+  }, {})
+}
+
+const getSuffix = index => {
+  return index > 0 ? `-${index}` : ``
 }
